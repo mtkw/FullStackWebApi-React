a4b2b289e79b11b31a1f155e73f938dc58d2f18c
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2025-03-01 16:19:44 +0100

    Toogle Dark Mode Implementation

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,12 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
-export default function NavBar() {
+type Props = {
+  toggleDarkMode: () => void;
+  darkMode: boolean;
+};
+
+export default function NavBar({ darkMode, toggleDarkMode }: Props) {
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Web Shop
-        </Typography>
+        <Typography variant="h6">Web Shop</Typography>
+        <IconButton onClick={toggleDarkMode}>
+          {darkMode ? <DarkMode /> : <LightMode sx={{ color: "yellow" }} />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
