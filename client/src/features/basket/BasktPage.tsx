e8e53f2d4a67902abf5e2f20de127a75f8a3c7d0
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2025-03-24 18:58:24 +0100

    Basket Page Leyout

---
+++ client/src/features/basket/BasktPage.tsx
@@ -0,0 +1,21 @@
+import { Grid2, Typography } from "@mui/material";
+import { useFetchBasketQuery } from "./basketApi";
+import BasketItem from "./BasketItem";
+
+export default function BasktPage() {
+  const { data, isLoading } = useFetchBasketQuery();
+
+  if (isLoading) return <Typography>Loading Basket...</Typography>;
+
+  if (!data) return <Typography variant="h3">Your Basket is empty</Typography>;
+
+  return (
+    <Grid2 container spacing={2}>
+      <Grid2 size={8}>
+        {data?.items.map((item) => (
+          <BasketItem item={item} key={item.id} />
+        ))}
+      </Grid2>
+    </Grid2>
+  );
+}
